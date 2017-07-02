f109fe5e5e04d202249757a9fe60d3dbc9561dfd
藪崎卓巳 <raru@syrup16g.net>
2017-07-03 03:29:10 +0900

    add sign up

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,20 +2,26 @@ package controllers
 
 import javax.inject._
 
+import jp.t2v.lab.play2.auth.OptionalAuthElement
 import play.api.data.Form
 import play.api.data.Forms.{ optional, text }
 import play.api.i18n.{ I18nSupport, MessagesApi }
 import play.api.mvc._
+import services.UserService
 
 /**
   * This controller creates an `Action` to handle HTTP requests to the
   * application's home page.
   */
 @Singleton
-class HomeController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
+class HomeController @Inject()(val userService: UserService, val messagesApi: MessagesApi)
+    extends Controller
+    with AuthConfigSupport
+    with OptionalAuthElement
+    with I18nSupport {
 
-  def index = Action { implicit request =>
-    Ok(views.html.index())
+  def index = StackAction { implicit request =>
+    Ok(views.html.index(loggedIn))
   }
 
 }
---
+++ app/forms/Login.scala
@@ -0,0 +1,6 @@
+package forms
+
+/**
+  * Created by raru on 2017/07/03.
+  */
+case class Login(email: String, password: String)
